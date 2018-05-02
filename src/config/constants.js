bd144f6853d022349e2ccd2a4421951225126a86
jeffrey lee <jeffreylee@kingyoorekismbp.dynamic.ucsd.edu>
2018-05-01 18:44:09 -0700

    changed api key

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,9 +1,9 @@
-export const FIREBASE_API_KEY = 'AIzaSyAVuxlbqfcKT-0O2DEq61MowySgrTGvjiE';
-export const FIREBASE_AUTH_DOMAIN = 'meetups-18695.firebaseapp.com';
-export const FIREBASE_DATABASE_URL = 'https://meetups-18695.firebaseio.com/';
-export const FIREBASE_PROJECT_ID = 'meetups-18695';
-export const FIREBASE_STORAGE_BUCKET = 'meetups-18695.appspot.com';
-export const FIREBASE_MESSAGING_SENDER_ID = '594948044745';
+export const FIREBASE_API_KEY = 'AIzaSyAMeF1Z5syis6vDTyHQtJ44Qh3_bGB2Gps';
+export const FIREBASE_AUTH_DOMAIN = 'letsmeet-a3cbe.firebaseapp.com';
+export const FIREBASE_DATABASE_URL = 'https://letsmeet-a3cbe.firebaseio.com';
+export const FIREBASE_PROJECT_ID = 'letsmeet-a3cbe';
+export const FIREBASE_STORAGE_BUCKET = 'letsmeet-a3cbe.appspot.com';
+export const FIREBASE_MESSAGING_SENDER_ID = '472038597943';
 
 export const GOOGLE_MAPS_API_KEY = 'AIzaSyAd3KucV9ap_X0cJmIHl37P4Hw0VI9eHQQ';
 export const GOOGLE_MAPS_PLACE_API_KEY = 'AIzaSyAOkeHdz33iLnUmkyWmoFoZ_B0otaz7ISY';
